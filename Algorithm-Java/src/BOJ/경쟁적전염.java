87b2150057139658ce6b8583dbdb0b8c540f0dd9
sunny413 <wjdrudfl413@gmail.com>
2024-01-05 15:03:05 +0900

    폴더 초기화

---
+++ Algorithm-Java/src/BOJ/경쟁적전염.java
@@ -0,0 +1,10 @@
+package BOJ;
+
+public class 경쟁적전염 {
+
+    BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer();
+    int N, K;
+    //st = bf.readLine();
+
+}
---
+++ Algorithm-Python/BOJ/경쟁적전염.py
@@ -0,0 +1,3 @@
+N,K = map(int, input().split())
+map= [list(map(int, input().split())) for _ in range(N)]
+print(N,K,map)
